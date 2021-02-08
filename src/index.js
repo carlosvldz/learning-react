b3738bd0f751bc515abeb51dff9c5810b130f96f
carlosvldz <me@carlosvldz.com>
2021-02-07 20:46:33 -0600

    add page BadgeNew

--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import './global.css';
-import Badge from './components/Badge';
+import BadgeNew from './pages/BadgeNew';
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
 ReactDOM.render(
-    <Badge
-        firstName="Carlos"
-        lastName="Sánchez"
-        avatarUrl="https://s.gravatar.com/avatar/4742948c26b62bc7b6ff5d00e0bcaffd?s=80"
-        jobTitle="Software Engineer"
-        twitter="carlosvldzzz"
+    <BadgeNew
+        
         />, container);
