dd2f4d370ffe5b9cb7233a1c06f041191f66eda2
carlosvldz <me@carlosvldz.com>
2021-02-18 01:00:24 -0600

    Add router and layout

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import './global.css';
-import BadgeNew from './pages/BadgeNew';
-import Badges from './pages/Badges';
+import App from './components/App';
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
 ReactDOM.render(
-    <Badges
-        
-        />, container);
+    <App />, container);
