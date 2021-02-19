a63cea34b9911dbb11204b4869053c0c32937a9f
carlosvldz <me@carlosvldz.com>
2021-02-19 12:56:12 -0600

    Add search filter with hooks

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -25,12 +25,44 @@ class BadgesListItem extends React.Component {
       );
     }
   }
+
+  function useSearchBadges(badges) {
+    const [query, setQuery] = React.useState('');
+    const [filteredBadges, setFilteredBadges] = React.useState(badges);
   
-  class BadgesList extends React.Component {
-    render() {
-      if (this.props.badges.length === 0) {
+    React.useMemo(() => {
+      const result = badges.filter(badge => {
+        return `${badge.firstName} ${badge.lastName}`
+          .toLowerCase()
+          .includes(query.toLowerCase());
+      });
+  
+      setFilteredBadges(result);
+    }, [badges, query]);
+  
+    return { query, setQuery, filteredBadges };
+  }
+  
+  function BadgesList (props) {
+    const badges = props.badges;
+
+    const { query, setQuery, filteredBadges } = useSearchBadges(badges);
+
+      if (filteredBadges.length === 0) {
         return (
           <div>
+            <div className="form-group">
+          <label>Filter Badges</label>
+          <input
+            type="text"
+            className="form-control"
+            value={query}
+            onChange={e => {
+              setQuery(e.target.value);
+            }}
+          />
+        </div>
+
             <h3>No badges were found</h3>
             <Link className="btn btn-primary" to="/badges/new">
               Create new badge
@@ -41,8 +73,20 @@ class BadgesListItem extends React.Component {
   
       return (
         <div className="BadgesList">
+          <div className="form-group">
+            <label>Filter Badges</label>
+            <input
+              type="text"
+              className="form-control"
+              value={query}
+              onChange={e => {
+                setQuery(e.target.value);
+          }}
+        />
+        </div>
+
           <ul className="list-unstyled">
-            {this.props.badges.map(badge => {
+            {filteredBadges.map(badge => {
               return (
                 <li key={badge.id}>
                   <Link
@@ -57,7 +101,6 @@ class BadgesListItem extends React.Component {
           </ul>
         </div>
       );
-    }
   }
   
   export default BadgesList;
